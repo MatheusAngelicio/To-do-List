ece165c05e4b5a4a6053439cdb32664106ac7d67
Matheus Angelicio <matheus.angelicio00@gmail.com>
2024-12-03 12:08:31 -0300

    Criando a tela Cadastro de Tarefa

--- app/src/main/java/com/example/todolist/ui/feature/ListScreen.kt
+++ app/src/main/java/com/example/todolist/ui/feature/ListScreen.kt
@@ -5,14 +5,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,7 +22,7 @@ import com.example.todolist.ui.theme.TodoListTheme
 
 @Composable
 fun ListScreen() {
-
+    ListContent(todos = emptyList())
 }
 
 @Composable
